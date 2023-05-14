eadce78541d6865ae69ffca444326ebd9fecc058
SalehBal <94306843+SalehBal@users.noreply.github.com>
2023-05-14 13:47:39 +0400

    Starting Work On The "Featured Posts" Part

---
+++ components/HomePage/FeaturedPosts/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import css from './FeaturedPosts.module.css';
+function FeaturedPosts() {
+    return (
+        <section className={css.latest}>
+            <h2>Featured Posts</h2>
+        </section>
+    );
+}
+
+export default FeaturedPosts;
---
+++ components/Posts/PostsGrid/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PostItem from './PostItem';
+import css from './PostsGrid.module.css';
+function PostsGrid({ posts }) {
+    return (
+        <ul className={css.grid}>
+            {posts.map((post) => (
+                <PostItem />
+            ))}
+        </ul>
+    );
+}
+
+export default PostsGrid;
