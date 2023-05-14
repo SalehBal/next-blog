65ba5fcc3a4151682ee25c47422fc13b62c4b85c
SalehBal <94306843+SalehBal@users.noreply.github.com>
2023-05-14 13:20:23 +0400

    Adding Layout & Navigation

---
+++ components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from './Navigation';
+function Layout({ children }) {
+    return (
+        <>
+            <Navigation />
+            <main>{children}</main>
+        </>
+    );
+}
+
+export default Layout;
